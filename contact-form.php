c0504ba7458a51cd3ba35477df10855b98ab0bdd
Lanastasi <68787523+Lanastasi@users.noreply.github.com>
2021-04-09 23:52:17 +0700

    Add files via upload

---
+++ contact-form.php
@@ -0,0 +1,23 @@
+<?php
+    $name = $_POST['name'];
+    $visitor_email = $_POST['email'];
+    $visitor_number = $_POST['phone'];
+    $message = $_POST['message'];
+
+    $email_from = 'Liiisss003@gmail.com';
+    $email_subject = 'New Form Submission';
+    $email_body = "User Name: $name.\n".
+                    "User Number: $phone.\n".
+                        "User Email: $visitor_email.\n".
+                            "User Message: $message.\n";
+
+    $to = "Liiisss003@gmail.com"
+
+    $headers = "From: $email_from \r\n";
+    $headers .= "Reply to: $visitor_email \r\n";
+    mail($to,$email_subject,$email_body,$headers);
+
+    header("Location: index.html");
+
+    
+?>
